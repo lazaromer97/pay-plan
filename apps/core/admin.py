57dfc700eb9bee3568bf1bf06a3635f5b0bb7f61
Lazaro Estrada <lazaromer97@gmail.com>
2021-06-12 16:40:07 -0400

    Create PaymentPlan model

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,3 +1,9 @@
+from django.db.models.expressions import F
+from apps.core.models import PaymentPlan
 from django.contrib import admin
 
+from .models import PaymentPlan
+
 # Register your models here.
+
+admin.site.register(PaymentPlan)
--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class PaymentPlan(models.Model):
+    name = models.CharField(verbose_name='Name', max_length=100, unique=True)
+    value = models.DecimalField(verbose_name='Value', max_digits=8, decimal_places=2)
+    recurring = models.BooleanField(verbose_name='Recurring', default=False)
+
+    def __str__(self):
+        self.name
+
+    class Meta:
+        db_table = 'payment_plan'
+        verbose_name = 'Payment Plan'
+        verbose_name_plural = 'Payment Plans'
